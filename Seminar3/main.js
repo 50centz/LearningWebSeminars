a28a13a7243eb2016c77cc20d10d45d2231150a1
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-01-31 21:16:32 +0300

    Work to work

---
+++ Seminar3/main.js
@@ -0,0 +1,58 @@
+
+
+// let num = 5;
+// console.log(num);
+
+// num = 10;
+// console.log(num);
+
+// function sum(a,b){
+//     return a + b
+// }
+
+// const num1 = Number.parseInt(prompt('Введите первое число'));
+// const num2 = Number.parseInt(prompt('Введите второе число'));
+// alert(`Резельтат сложения чисел ${num1} и ${num2} равно ${num1 + num2}`);
+
+
+// function name(name){
+    
+//     console.log(`Привет, ${name}`);
+// }
+// const userName = prompt('Введите ваше имя')
+// name(userName);
+
+
+// const u = confirm("Вам хорошо живёться ?");
+// if (u) {
+//     alert("Тогда мы идём к вам");
+// } else {
+//     alert("Ну вы держитесь там!");
+// }
+
+
+// alert(confirm("Вам хорошо живёться ?") ? "Тогда мы идём к вам": "Ну вы держитесь там!");
+
+
+// Задача 5: перепишите код, используя конструкцию switch-case, запрашивая продукт через диалоговое окно.
+ 
+// <script>
+// let product = "Бананы";
+ 
+// if (product == "Мандарины") {
+//   alert('Мандарины стоят 100 руб/кг.');
+// } else if (product == "Бананы") {
+//   alert('Бананы и груши стоят 70 руб/кг.');
+// } else if (product == "Груши") {
+//   alert('Бананы и груши стоят 70 руб/кг.');
+// } else {
+//   alert('Нет такого продукта.');
+// }
+// </script>
+
+// const product = "Бананы";
+
+// switch(product){
+//     case 'Мандарины':
+//         alert
+// }
