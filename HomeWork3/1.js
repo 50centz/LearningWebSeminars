04594c252289cb10f34366c56d4e6a25a5c2139d
Федоров Евгений <zen-g@mail.ru>
2023-02-01 15:14:33 +0300

    Solved third homework

---
+++ HomeWork3/1.js
@@ -0,0 +1,14 @@
+while(true){
+    digit = parseFloat(prompt('Введите температуру в градусах Цельсия: '));
+    if (digit < 1000000000000000){
+        break;
+    }
+}
+
+// digit = parseFloat(prompt('Введите температуру в градусах Цельсия: '));
+a = 9/5;
+far = a * digit +32;
+far = Math.round(far *10) /10;
+alert(`Цельсий: ${digit}, Фаренгейт: ${far}`);
+
+
